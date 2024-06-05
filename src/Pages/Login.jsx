1f9e4647349226efbb4d0cfaf5f4f289b1f42fee
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-06 00:29:45 +0530

    Completed Login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React from "react";
+import { RxCross2 } from "react-icons/rx";
+import { IoCallOutline } from "react-icons/io5";
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div
+      div
+      className="w-screen h-auto loginBg flex flex-col justify-center items-center py-10"
+    >
+      <div className="w-[500px] h-[650px] bg-white rounded-[50px]">
+        {/* Vector  */}
+        <img src="/Images/plus-vector.svg" alt="" className="pt-10 pl-10" />
+
+        {/* Form Content  */}
+        <div className="flex flex-col justify-center items-center ">
+          <img
+            src="/Images/logo.png"
+            alt=""
+            className="flex justify-center items-center"
+          />
+        {/* <img src="/Images/Vector3.svg" alt="" className="" /> */}
+
+          <h1 className="poppins-extrabold text-[#1f6acb] py-5 text-[20px]">
+            Report Download Portal
+          </h1>
+          {/* Inner Container  */}
+          <div className="bg-[#00d3eb] w-[400px] h-[300px] rounded-[20px] flex flex-col items-center">
+            <h1 className="poppins-regular text-[20px]  text-[#2b5388] pt-5">
+              Login
+            </h1>
+            {/* Form */}
+            <form action="" className="flex flex-col">
+              <label
+                for="name"
+                className="poppins-regular text-[#2b5388] text-[15px]"
+              >
+                Username
+              </label>
+              <input
+                className="w-[300px] h-[40px] rounded-[5px] px-2"
+                type="text"
+                name="name"
+                placeholder="Enter email Id"
+                id="name"
+              />
+              <label
+                for="password"
+                className="poppins-regular text-[#2b5388] text-[15px]"
+              >
+                Password
+              </label>
+              <input
+                className="w-[300px] h-[40px] rounded-[5px] px-2"
+                type="text"
+                name="name"
+                placeholder="Password"
+              />
+              <div className="w-[300px] h-[50px] flex flex-col items-center pt-5">
+                <button
+                  type="submit"
+                  className="w-[120px] h-[50px]  bg-[#1f6cab] text-white poppins-regular rounded-[5px]"
+                >
+                  SUBMIT
+                </button>
+              </div>
+            </form>
+            <h1 className="poppins-semibold text-[14px] text-[#505050] underline pt-5">
+              Reset Password
+            </h1>
+          </div>
+          {/* Mobile number & privacy Policy  */}
+          <div className="flex justify-center items-center   text-[#1f6acb] py-5 text-[20px]">
+        {/* <img src="/Images/Vector2.svg" alt="" className="pr-10" /> */}
+            <IoCallOutline className="text-[25px]" />
+            <h1 className="poppins-bold">(+91)9288008801</h1>
+          </div>
+          <p className="poppins-semibold text-[12px] text-[#505050]">
+            I hereby agree and accept the{" "}
+            <span className="text-[#1f6acb] cursor-pointer">
+              Terms of service
+            </span>{" "}
+            and{" "}
+            <span className="text-[#1f6acb] cursor-pointer">
+              Privacy policy
+            </span>
+          </p>
+        </div>
+      </div>
+
+      {/* Copyright & Privacy Policy */}
+      <div className=" w-screen text-white flex flex-row justify-around pt-5 poppins-semibold text-[12px]">
+        <h1>Copyright © 2024 Access Home Lab Solutions</h1>
+        <h1>All Rights Reserved  | <span className="text-[#1f6acb] cursor-pointer">Terms and Conditions</span> | <span className="text-[#1f6acb] cursor-pointer">Privacy Policy</span></h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
