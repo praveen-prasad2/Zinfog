34a546e8a98cfc55561b655fe3b1a20f305d49c9
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-05 21:43:58 +0530

    Updated code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html","./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
