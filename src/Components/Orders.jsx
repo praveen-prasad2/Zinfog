dca1b9e7f9d5ef08c08230baf660023231fa2c2c
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-07 04:47:07 +0530

    Updated Navigation Styles of React Paginate

--- src/Components/Orders.jsx
+++ src/Components/Orders.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactPaginate from "react-paginate";
 import OrderCard from "./OrderCard";
 import orderItems from "../utils/orderItems";
+import { FaCircleChevronLeft } from "react-icons/fa6";
+import { FaCircleChevronRight } from "react-icons/fa6";
 
 function Orders() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 10; // Number reports in one page
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const offset = currentPage * itemsPerPage;
+  const pageCount = Math.ceil(orderItems.length / itemsPerPage);
+
   return (
     <>
-    
-    <div className="bg-white mx-5 mt-5 border-2 border-b-black">
-      {/* Order Categories Header  */}
-      <div className="flex flex-row justify-between px-5 items-center poppins-semibold">
+      <div className="bg-white mx-5 mt-5 border-2 border-b-black">
+        {/* Order Categories Header */}
+        <div className="flex flex-row justify-between px-5 items-center poppins-semibold">
+          {/* Header items */}
           <div className="header-item">
             <h1>Order Number</h1>
           </div>
@@ -37,20 +50,39 @@ function Orders() {
             <h1>Action</h1>
           </div>
         </div>
-    </div>
-    {/* order Cards  */}
-    <div>
-        {
-            orderItems.map((item)=>(
-                <div key={item.id}>
-                    
-                    <OrderCard item={item}/>
-                </div>
-            ))
-        }
-    </div>
+      </div>
+      {/* Order Cards */}
+      <div className="">
+        {orderItems.slice(offset, offset + itemsPerPage).map((item) => (
+          <div key={item.id}>
+            <OrderCard item={item} />
+          </div>
+        ))}
+      </div>
+      {/* Pagination */}
+      <div className="bg-white pagination-container flex flex-row justify-center items-center py-5 poppins-semibold mx-5">
+        <ReactPaginate
+          previousLabel={
+            <FaCircleChevronLeft className="text-[30px] text-zinfog-primary" />
+          } 
+          nextLabel={
+            <FaCircleChevronRight className="text-[30px] text-zinfog-primary" />
+          } 
+          breakLabel={"..."}
+          pageCount={pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageChange}
+          activeClassName={"active"}
+          previousClassName={"pagination-prev"} 
+          nextClassName={"pagination-next"} 
+          pageClassName={"pagination-page"} 
+          disabledClassName={"pagination-disabled"} 
+          activeLinkClassName={"pagination-active-link"} 
+          className="flex flex-row items-center  gap-3"
+        />
+      </div>
     </>
-    
   );
 }
 
