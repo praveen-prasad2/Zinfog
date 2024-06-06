c8d58205a333c68881921608677a5e11b9f0f593
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-07 02:39:57 +0530

    Updated orders card  and orders

--- src/Components/OrderCard.jsx
+++ src/Components/OrderCard.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import { FaDownload } from "react-icons/fa";
 import { IoChatbox } from "react-icons/io5";
 
-function OrderCard() {
+function OrderCard({status}) {
+    // Function to change the status background color based on the status
+  const getStatusClass = (status) => {
+    switch (status) {
+      case 'Ready':
+        return 'bg-[#89ffaa]';
+      case 'Partial Report':
+        return 'bg-[#e7f880]';
+      case 'Lab Dropped':
+        return 'bg-[#f2a38a]';
+      default:
+        return 'bg-gray-400';
+    }}
   return (
+    <>
+    
     <div className="bg-white mx-5 text-[14px]">
-         <div className="flex flex-row justify-between px-5 h-[50px] items-center poppins-regular">
+         <div  className="flex flex-row justify-between px-5  items-center poppins-regular">
           <div className="header-item">
             <h1>A19023</h1>
           </div>
@@ -27,19 +41,19 @@ function OrderCard() {
           <div className="header-item">
             <h1>03/05/2023</h1>
           </div>
-          <div className="header-item">
-            <h1>Ready</h1>
+          <div className="header-item" id="status"  >
+            <h1 className={`${getStatusClass(status)} px-3 py-1 w-32 rounded-[50px] poppins-regular text-zinfog-black`}>{status}</h1>
           </div>
-          <div className="header-item">
-            <h1>Action</h1>
+          <div className="header-item flex flex-row text-[20px] text-zinfog-primary gap-5">
+          <FaDownload />
+          <IoChatbox />
           </div>
         </div>
       
-        {/* <div className="flex flex-row text-[20px] text-zinfog-primary gap-5">
-          <FaDownload />
-          <IoChatbox />
-        </div> */}
+       
       </div>
+   
+    </>
   );
 }
 
--- src/Components/Orders.jsx
+++ src/Components/Orders.jsx
@@ -7,11 +7,11 @@ function Orders() {
     
     <div className="bg-white mx-5 mt-5 border-2 border-b-black">
       {/* Order Categories Header  */}
-      <div className="flex flex-row justify-between px-5 h-[50px] items-center poppins-semibold">
+      <div className="flex flex-row justify-between px-5 items-center poppins-semibold">
           <div className="header-item">
             <h1>Order Number</h1>
           </div>
-          <div className="header-item">
+          <div className="w-[100px] header-item">
             <h1>Date</h1>
           </div>
           <div className="header-item">
@@ -39,7 +39,9 @@ function Orders() {
     </div>
     {/* order Cards  */}
     <div>
-        <OrderCard/>
+        <OrderCard status={"Ready"}/>
+        <OrderCard status={"Partial Report"}/>
+        <OrderCard status={"Lab Dropped"}/>
     </div>
     </>
     
