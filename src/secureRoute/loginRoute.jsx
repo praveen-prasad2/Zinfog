14a8c66875bf5aeba98a317709ea6bad1ef0ea36
Praveen Prasad <praveenprasad2510@gmail.com>
2024-06-08 01:00:37 +0530

    Added User authentication in Login

---
+++ src/secureRoute/loginRoute.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+// import { Navigate } from "react-router-dom";
+// import { useAuth } from "../context/authContext";
+
+// const LoginRoute = ({ children }) => {
+//   const { auth } = useAuth();
+//   if (!auth.token) {
+//     return <Navigate to="/login" />;
+//   }
+//   return children;
+// };
+
+// export default LoginRoute;
